4f7feee14790062a75bcc2085a2296788121430b
Richard Liu <37211328+richardliuca@users.noreply.github.com>
2018-09-28 01:53:32 -0700

    Revision_2

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 from numpy import *
 from scipy.optimize import curve_fit
 from scipy.signal import medfilt, butter, filtfilt, freqz, get_window
+from scipy.stats
 
-# Read  Me
+# Read Me
 # Specify the main directory below under workDir
 # Then place data in the specific order inside the main directory : Sensor/Scenario/execfile
 # For example: Strain_Gauge/SG_dyn/dv1.xlsx
@@ -36,6 +37,9 @@ for sensor in senList:
         # Change to scenario folders
         os.chdir(senDir + '/' + folderName)
         print('Now in {}'.format(senDir+'/'+folderName))
+
+        residual = zeros((3, 50000))
+        xData = zeros((3, 50000))
 
         # Iterating through 3 trials
         for trial in range(1,4):
@@ -93,11 +97,19 @@ for sensor in senList:
                 totalDisplacement-=shift
 
             def truncate():
-                global time, voltage, totalDisplacement
+                global time, voltage, totalDisplacement, sensor
                 indexBool = time>=1
                 voltage = voltage[indexBool]
-                totalDisplacement = totalDisplacement[indexBool]
                 time = time[indexBool]
+                if sensor == 'Accelerometer':
+                    global acceleration
+                    acceleration = acceleration[indexBool]
+                elif sensor == 'Voice_Coil':
+                    global velocity
+                    velocity = velocity[indexBool]
+                else:
+                    totalDisplacement = totalDisplacement[indexBool]
+                return indexBool
 
             def numerical_diff(x, dt, scale):
                 global low_pass_filter
@@ -129,7 +141,7 @@ for sensor in senList:
 
             # Curve fitting based on scenario
             def checkScenario(f, x, y):
-                global report, trial, sensor
+                global report, trial, sensor, residual, xData
 
                 if 'static' in folderName:
                     y = low_pass_filter(y, cutoff = 10)
@@ -143,8 +155,12 @@ for sensor in senList:
                     report[sensor]['quasi_static'].append(popt)
 
                 elif 'trans' in folderName:
-                    truncate()
-                    popt, _ = curve_fit(f, x, y)
+                    index = truncate()
+                    if len(residual[0]) == 50000:
+                        residual = zeros((3, len(x[index])))
+                        xData = zeros((3, len(x[index])))
+
+                    popt, _ = curve_fit(f, x[index], y[index])
                     report[sensor]['transient'].append(popt)
 
                 elif 'dyn' in folderName:
@@ -155,87 +171,157 @@ for sensor in senList:
                 else:
                     raise ValueError('Scenario Not Found ')
 
+                return popt
+
+            # Plotting
+            def plottingSubplot3(x1, y1, x2, y2, x3, y3, x4, y4,
+                                xLabels, yLabels, labels):
+
+                global sensor, folderName, trial
+                plt.figure(figsize = (14, 7))
+
+                plt.subplot(311)
+                plt.gca().set_title('Sensor: {} Case : {} Trial {}'.format(
+                sensor, folderName, str(trial)))
+                plt.plot(x1, y1, label = labels[0])
+                plt.xlabel(xLabels[0])
+                plt.ylabel(yLabels[0])
+                plt.grid(b = True, which = 'both')
+                plt.legend()
+
+                plt.subplot(312)
+                plt.plot(x2, y2, label = labels[1])
+                plt.xlabel(xLabels[1])
+                plt.ylabel(yLabels[1])
+                plt.grid(b = True, which = 'both')
+                plt.legend()
+
+                plt.subplot(313)
+                plt.plot(x3, y3, label = labels[2])
+                plt.plot(x4, y4, 'r', label = 'Fitted Curve')
+                plt.xlabel(xLabels[2])
+                plt.ylabel(yLabels[2])
+                plt.grid(b = True, which = 'both')
+                plt.legend()
+
+                plt.savefig('Trial_{}.png'.format(trial))
+                # plt.show()
+                plt.close()
+
             # Data Anaysis
             if sensor == 'Strain_Gauge':
                 # Governing equation
                 # Vout = Vin + g*IR*delta_L + I*R
-
                 sg = lambda x, a, b: 5 + 2*a*x + b
-                checkScenario(sg, totalDisplacement, voltage)
+                coefficient = checkScenario(sg, totalDisplacement, voltage)
 
-                # sg = lambda x, a, b: a*x + b
-                # checkScenario(sg, voltage, totalDisplacement)
+                fit = sg(totalDisplacement, coefficient[0], coefficient[1])
+
+                yBar = sum(voltage)/len(voltage)
+                ssreg = sum((fit - yBar)**2)
+                sstot = sum((voltage - yBar)**2)
+
+                print('R square is : {}'.format(ssreg/sstot))
+
+                residual[trial-1] = voltage - fit
+
+                xData[trial-1] = totalDisplacement
+
+                plottingSubplot3(time, voltage, time, totalDisplacement,
+                            totalDisplacement, voltage,
+                            totalDisplacement, fit,
+                            ['Time (s)', 'Time (s)', 'Distance (m)'],
+                            ['Voltage (v)', 'Distance (m)', 'Voltage (v)'],
+                            [sensor, 'Encoder', 'data'])
 
             elif sensor == 'IR':
                 # Governing equation
-                ir = lambda x, a, b: a*exp(b*x)
-                checkScenario(ir, totalDisplacement, voltage)
+                ir = lambda x, a, b: a*x + b
+                coefficient = checkScenario(ir, totalDisplacement, voltage)
 
-                # ir = lambda x, a, b: a*sqrt(x) + b
-                # checkScenario(ir, voltage, totalDisplacement)
+                fit = ir(totalDisplacement, coefficient[0], coefficient[1])
+
+                yBar = sum(voltage)/len(voltage)
+                ssreg = sum((fit - yBar)**2)
+                sstot = sum((voltage - yBar)**2)
+
+                print('R square is : {}'.format(ssreg/sstot))
+
+                residual[trial-1] = voltage - fit
+
+                xData[trial-1] = totalDisplacement
+
+                plottingSubplot3(time, voltage, time, totalDisplacement,
+                            totalDisplacement, voltage,
+                            totalDisplacement, fit,
+                            ['Time (s)', 'Time (s)', 'Distance (m)'],
+                            ['Voltage (v)', 'Distance (m)', 'Voltage (v)'],
+                            [sensor, 'Encoder', 'data'])
 
             elif sensor == 'Voice_Coil':
-                pass
-                # velocity, _ = numerical_diff(totalDisplacement,
-                #                             dt = time[1]-time[0], scale = 40)
-                # plt.figure(5)
-                # plt.title('Encoder Velocity')
-                # plt.plot(time, velocity, 'b')
-                # plt.xlabel('Time (s)')
-                # plt.ylabel('Velocity (mm/s)')
-                # plt.show()
+                velocity, _ = numerical_diff(totalDisplacement,
+                                            dt = time[1]-time[0], scale = 40)
+                # Governing Equation
+                vc = lambda x, a, b: a*x + b
+                coefficient = checkScenario(vc, velocity, voltage)
+
+                fit = vc(velocity, coefficient[0], coefficient[1])
+
+                yBar = sum(voltage)/len(voltage)
+                ssreg = sum((fit - yBar)**2)
+                sstot = sum((voltage - yBar)**2)
+
+                print('R square is : {}'.format(ssreg/sstot))
+
+                residual[trial-1] = voltage -fit
+
+                xData[trial-1] = velocity
+
+                plottingSubplot3(time, voltage, time, velocity,
+                            velocity, voltage, velocity, fit,
+                            ['Time (s)', 'Time (s)', 'Velocity (m/s)'],
+                            ['Voltage (v)', 'Velocity (m/s)', 'Voltage (v)'],
+                            [sensor, 'Encoder', 'data'])
 
             elif sensor == 'Accelerometer':
-                pass
-                # velocity, _ = numerical_diff(totalDisplacement,
-                #                             dt = time[1]-time[0], scale = 40)
-                # acceleration, _ = numerical_diff(velocity,
-                #                     dt = time[1]-time[0], scale = 40)
-                # plt.figure(4)
-                # plt.title('Encoder Acceleration')
-                # plt.plot(time, acceleration, 'b')
-                # plt.xlabel('Time (s)')
-                # plt.ylabel('Acceleration (mm s^-2)')
-                # plt.show()
+                velocity, _ = numerical_diff(totalDisplacement,
+                                            dt = time[1]-time[0], scale = 40)
+                acceleration, _ = numerical_diff(velocity,
+                                    dt = time[1]-time[0], scale = 40)
+                # Governing Equation
+                acc = lambda x, a, b: a*x + b
+                coefficient = checkScenario(acc, acceleration, voltage)
+
+                fit = acc(acceleration, coefficient[0], coefficient[1])
+
+                yBar = sum(voltage)/len(voltage)
+                ssreg = sum((fit - yBar)**2)
+                sstot = sum((voltage - yBar)**2)
+
+                print('R square is : {}'.format(ssreg/sstot))
+
+                residual[trial-1] = voltage - fit
+
+                xData[trial-1] = acceleration
+
+                plottingSubplot3(time, voltage, time, acceleration,
+                        acceleration, voltage, acceleration, fit,
+                        ['Time (s)', 'Time (s)', 'Acceleration (m s^-2)'],
+                        ['Voltage (v)', 'Acceleration (m s^-2)', 'Voltage (v)'],
+                        [sensor, 'Encoder', 'data'])
 
             else:
                 raise ValueError('Sensor Not Regonized')
 
-            # Plotting
-            plt.figure(figsize = (13, 7.5))
-            # plt.subplots(3,1)
+        plt.figure(figsize = (14, 7))
+        plt.title('Residual : {}'.format(folderName))
+        plt.plot(xData[0], residual[0], ':', label = 'Trial 1')
+        plt.plot(xData[1], residual[1], ':', label = 'Trial 2')
+        plt.plot(xData[2], residual[2], ':', label = 'Trial 3')
+        plt.grid(b = True, which = 'both')
+        plt.legend()
+        plt.savefig('Residual.png')
+        # plt.show()
+        plt.close()
 
-            plt.subplot(311)
-            plt.gca().set_title('Sensor: {} Case : {} Trial {}'.format(
-            sensor, folderName, str(trial)))
-            plt.plot(time, voltage, label = '{}'.format(sensor))
-            plt.xlabel('Time')
-            plt.ylabel('Voltage')
-            plt.legend()
-
-            plt.subplot(312)
-            plt.plot(time, totalDisplacement, label = 'Encoder')
-            plt.xlabel('Time')
-            plt.ylabel('Distance')
-            plt.legend()
-
-            plt.subplot(313)
-            plt.plot(totalDisplacement, voltage, label = 'curve fit')
-            # plt.plot(voltage, totalDisplacement, label = 'curve fit')
-            # plt.xlabel('Voltage')
-            plt.xlabel('Distance')
-            # plt.ylabel('Distance')
-            plt.ylabel('Voltage')
-            plt.legend()
-
-            plt.savefig('Trial_{}'.format(trial))
-            # plt.show()
-            plt.close()
-
-    # Calculating Means
-    # if sensor == 'Strain_Gauge':
-    #     report[sensor]['static'] = append(report[sensor]['static'], average(report[sensor]['static']))
-    #     report[sensor]['quasi_static'] = append(report[sensor]['quasi_static'], average(report[sensor]['quasi_static']))
-    #     report[sensor]['transient'] = append(report[sensor]['transient'], average(report[sensor]['transient']))
-    #     report[sensor]['dynamic'] = append(report[sensor]['dynamic'], average(report[sensor]['dynamic']))
     print(report[sensor])
